144152b1a30920efd41db26836467d351ed3af37
ibking20 <47895855+ibking20@users.noreply.github.com>
2021-05-21 19:56:24 +0000

    Update frequentiel.py

--- frequentiel.py
+++ frequentiel.py
@@ -1,6 +1,5 @@
 
 
-frequences = [9.42,1.02,2.64,3.39,15.87,0.95,1.04,0.77,8.41,0.89,0.00,5.34,3.24,7.15,5.14,2.86,1.06,6.46,7.90,7.26,6.24,2.15,0.00,0.30,0.24,0.32]
 lettres = "EAISTNRULODMPCVQGBFJHZXYKW"
 def count(texte):
   alphabet = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
